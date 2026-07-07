b64c0466dad3d83a97a870b8563671f959434ba0
yusufashiru410-png <yusufashiru410@gmail.com>
2026-07-07 06:13:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,18 @@ function validateForm() {
 
     alert("Message sent successfully!");
     return true;
-}// Dark Mode
+}// Apply saved theme
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+// Dark Mode
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
 }
